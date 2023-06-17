35e8befd8fadbbbc1600c462394ecd4f8d8fe6ca
emon al hasan <emonalhasan41@gmail.com>
2023-06-17 14:53:00 +0600

    nodemailer

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const nodemailer=require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport');
+
+
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -40,17 +45,79 @@ function verifyJWT(req, res, next) {
   })
 }
 
+const  emailSenderOptions={
+  auth:{
+    
+    api_key:process.env.EMAIL_SENDER_KEY
+  }
+}
+
+
+var emailClient = nodemailer.createTransport(sgTransport(emailSenderOptions));
+
+function sendAppointmentEmail(booking){
+
+const {patient,patientName,treatment,date,slot}=booking; 
+
+var email = {
+  from:	's1811077141@ru.ac.bd',
+  to:patient,
+  subject:`Your Appointment for ${treatment} is on ${date} at ${slot} is Confirmed`,
+  text:`Your Appointment for ${treatment} is on ${date} at ${slot} is Confirmed`,
+  html:`
+  <div>
+  <p>Hello ${patientName}</p>
+  <h3>Your Appointment for ${treatment} is confirmed</h3>
+  <p>Looking forward to seeing you on  ${date} at ${slot}</p>
+  <h3>Our Address</h3>
+  <p>Mathihar,Rajshahi,Bangladesh</p>
+  <p>Thanks a lot for Booking us </P>
+
+  </div>
+  `
+};
+emailClient.sendMail(email, function(err, res) {
+  if (err) { 
+      console.log(err) 
+  }
+  console.log(res);
+});
+
+
+}
+
+
+
+
+
+
 async function run() {
   try {
     await client.connect();
     const servicesCollection = client.db('ru_doctors').collection('services');
     const bookingCollection = client.db('ru_doctors').collection('bookings');
     const userCollection = client.db('ru_doctors').collection('users');
+    const doctorCollection = client.db('ru_doctors').collection('doctors');
+
+
+  const verifyAdmin=async(req,res,next)=>{
+    const requester=req.decoded.email;
+    const requesterAccount=await userCollection.findOne({email:requester})
+   
+    if(requesterAccount.role === 'admin')
+    {
+      next()
+    }
+    else{
+      res.status(403).send({message:'forbiden'})
+    }
+  }
+
 
 
     app.get('/service', async (req, res) => {
       const query = {};
-      const cursor = servicesCollection.find(query)
+      const cursor = servicesCollection.find(query).project({name:1})
       const services = await cursor.toArray();
       res.send(services);
     })
@@ -69,28 +136,19 @@ async function run() {
     res.send({admin:isAdmin})
   })
 
-  app.put('/user/admin/:email',verifyJWT, async (req, res) => {
+  app.put('/user/admin/:email',verifyJWT,verifyAdmin, async (req, res) => {
     const email = req.params.email;
-    const requester=req.decoded.email;
-    const requesterAccount=await userCollection.findOne({email:requester})
-   
-    if(requesterAccount.role === 'admin')
-    {
     
     
       const filter = { email: email };
-    
+
       const updateDoc = {
         $set: {role :'admin'},
       };
       const result = await userCollection.updateOne(filter, updateDoc)
       
       res.send( result)
-    }
-    else{
-      res.status(403).send({message:'forbiden'})
-    }
-    
+   
   })
 
 
@@ -159,8 +217,32 @@ async function run() {
       }
 
       const result = await bookingCollection.insertOne(booking)
+      console.log('sending email')
+      sendAppointmentEmail(booking)
       return res.send({ success: true, result });
-    })
+    });
+
+app.get('/doctor',verifyJWT,verifyAdmin,async(req,res)=>{
+  const doctors=await doctorCollection.find().toArray();
+  res.send(doctors)
+})
+
+
+
+app.post('/doctor',verifyJWT,verifyAdmin,async(req,res)=>{
+  const doctor=req.body;
+  const result=await doctorCollection.insertOne(doctor);
+  res.send(result);
+})
+
+
+app.delete('/doctor/:email',verifyJWT,verifyAdmin,async(req,res)=>{
+  const email=req.params.email;
+  const filter={email:email}
+  const result=await doctorCollection.deleteOne(filter);
+  res.send(result);
+})
+
   }
   finally {
 
